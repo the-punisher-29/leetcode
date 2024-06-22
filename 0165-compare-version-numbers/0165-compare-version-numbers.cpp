f10174b747b91ecf777f6451c18ca262269f6d21
Soumen Kumar <140332900+the-punisher-29@users.noreply.github.com>
2024-06-22 19:08:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (24.74%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        // Split the version strings by '.'
+        vector<int> v1 = splitVersion(version1);
+        vector<int> v2 = splitVersion(version2);
+        
+        // Compare the revisions
+        int n = max(v1.size(), v2.size());
+        for (int i = 0; i < n; i++) {
+            int num1 = (i < v1.size()) ? v1[i] : 0;
+            int num2 = (i < v2.size()) ? v2[i] : 0;
+            
+            if (num1 < num2) return -1;
+            else if (num1 > num2) return 1;
+        }
+        
+        // Both versions are equal
+        return 0;
+    }
+    
+private:
+    vector<int> splitVersion(string version) {
+        vector<int> revisions;
+        istringstream iss(version);
+        string revision;
+        while (getline(iss, revision, '.')) {
+            revisions.push_back(stoi(revision));
+        }
+        return revisions;
+    }
+};
