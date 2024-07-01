7025c6fb70f13be9bcf621b85e814cc262f63d56
Soumen Kumar <140332900+the-punisher-29@users.noreply.github.com>
2024-07-02 00:02:59 +0530

    Time: 8 ms (26.63%), Space: 13.3 MB (23.69%) - LeetHub

---
+++ 0985-bag-of-tokens/0985-bag-of-tokens.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+        int score = 0, maxScore = 0;
+        int left = 0, right = tokens.size() - 1;
+        
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                power -= tokens[left++];
+                maxScore = max(maxScore, ++score);
+            } else if (score > 0) {
+                power += tokens[right--];
+                score--;
+            } else {
+                break;
+            }
+        }
+        
+        return maxScore;
+    }
+};
