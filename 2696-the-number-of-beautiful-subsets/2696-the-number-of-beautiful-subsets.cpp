1be2ff52871893a64b51f0ba9cb74c5a357365d1
Soumen Kumar <140332900+the-punisher-29@users.noreply.github.com>
2024-07-07 00:01:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int beautifulSubsets(std::vector<int>& nums, int k) {
-        int n = nums.size();
-        unordered_map<int, int> count;
-        return dfs(nums, k, 0, count) - 1; // Subtract 1 to exclude the empty subset
-    }
+    vector<int> leftRightDifference(vector<int>& nums) {
+        vector<int> leftSum(nums.size()),rightSum(nums.size()),answer(nums.size());
 
-private:
-    int dfs(const vector<int>& nums, int k, int index, unordered_map<int, int>& count) {
-        if (index == nums.size()) {
-            return 1; // This subset is valid
+        int sum=0;
+        for(int i=0;i<nums.size();++i){
+            leftSum[i]=sum;
+            sum=sum+nums[i];
         }
-        
-        // Option 1: Skip the current element
-        int totalCount = dfs(nums, k, index + 1, count);
 
-        // Option 2: Include the current element if it's valid
-        int current = nums[index];
-        if (count[current - k] == 0 && count[current + k] == 0) {
-            count[current]++;
-            totalCount += dfs(nums, k, index + 1, count);
-            count[current]--;
+        sum=0;
+        for(int i=nums.size()-1;i>=0;--i){
+            rightSum[i]=sum;
+            sum=sum+nums[i];
         }
-        
-        return totalCount;
+
+        for(int i=0;i<nums.size();++i){
+            answer[i]=abs(leftSum[i]-rightSum[i]);
+        }
+        return answer; 
     }
 };
