08500ee1bd4f01c4fdd47ccb1dcfe1c781cd3d92
Soumen Kumar <soumenkumar9503@gmail.com>
2024-08-03 20:41:45 +0530

    Time: 8 ms (66.96%), Space: 17.6 MB (42.71%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        int m=target.size();
+        int n=arr.size();
+        if(m!=n) return false;
+        vector<int> freq(1001,0);
+        for(int i=0;i<m;++i){
+            freq[target[i]]++;
+            freq[arr[i]]--;
+        }
+        for(int count:freq){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
