80da4b72a9fd2529b534912eef84a0fe209d61f4
Soumen Kumar <soumenkumar9503@gmail.com>
2024-08-21 03:59:23 +0530

    Time: 4 ms (32.07%), Space: 10.1 MB (45.99%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,21 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //rec
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         inorder(root, result);
+//         return result;
+//     }
+
+// private:
+//     void inorder(TreeNode* node, vector<int>& result) {
+//         if (node == nullptr) {
+//             return;
+//         }
+//         inorder(node->left, result);
+//         result.push_back(node->val);
+//         inorder(node->right, result);
+//     }
+// };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
-        inorder(root, result);
-        return result;
-    }
-
-private:
-    void inorder(TreeNode* node, vector<int>& result) {
-        if (node == nullptr) {
-            return;
+        if (root == nullptr) {
+            return result;
         }
-        inorder(node->left, result);
-        result.push_back(node->val);
-        inorder(node->right, result);
+        
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        
+        while (curr != nullptr || !st.empty()) {
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            result.push_back(curr->val);
+            curr = curr->right;
+        }
+        
+        return result;
     }
 };
