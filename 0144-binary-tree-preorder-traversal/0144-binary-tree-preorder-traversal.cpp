e38cc5e9203af361ce2e29c5c38c92def64e1233
Soumen Kumar <soumenkumar9503@gmail.com>
2024-08-21 03:58:11 +0530

    Time: 0 ms (100%), Space: 10.1 MB (24.21%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,20 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //recur
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int>res;
+//         po(root,res);
+//         return res;
+//     }
+// private:
+//    void po(TreeNode *node,vector<int> &res){
+//     if(node==NULL){
+//         return;
+//     }
+//     res.push_back(node->val);
+//     po(node->left,res);
+//     po(node->right,res);
+//    }
+// };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>res;
-        po(root,res);
-        return res;
+        vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        stack<TreeNode*> st;
+        st.push(root);
+        
+        while (!st.empty()) {
+            TreeNode* curr = st.top();
+            st.pop();
+            result.push_back(curr->val);
+            
+            if (curr->right) {
+                st.push(curr->right);
+            }
+            if (curr->left) {
+                st.push(curr->left);
+            }
+        }
+        
+        return result;
     }
-private:
-   void po(TreeNode *node,vector<int> &res){
-    if(node==NULL){
-        return;
-    }
-    res.push_back(node->val);
-    po(node->left,res);
-    po(node->right,res);
-   }
 };
