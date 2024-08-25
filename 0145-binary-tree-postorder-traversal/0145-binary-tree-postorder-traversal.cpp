d1f826e9ab52ee5d09d2aa5dec239ddba293c25d
Soumen Kumar <soumenkumar9503@gmail.com>
2024-08-25 20:55:20 +0530

    Time: 0 ms (100%), Space: 10.1 MB (44.35%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,20 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //recur sol---trivial
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         posto(root,res);
+//         return res;
+//     }
+// private:
+//     void posto(TreeNode* node,vector<int> &res){
+//         if(node==NULL){
+//             return;
+//         }
+//         posto(node->left,res);
+//         posto(node->right,res);
+//         res.push_back(node->val);
+//     }
+// };
+
+//iter sol---interes
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
-        posto(root,res);
-        return res;
-    }
-private:
-    void posto(TreeNode* node,vector<int> &res){
-        if(node==NULL){
-            return;
+        if (root == NULL) {
+            return res;
         }
-        posto(node->left,res);
-        posto(node->right,res);
-        res.push_back(node->val);
+        stack<TreeNode*> st;
+        TreeNode *curr = root;
+        TreeNode *lastVisited = NULL;
+        while (!st.empty() || curr != NULL) {
+            if (curr != NULL) {
+                st.push(curr);
+                curr = curr->left;
+            } else {
+                TreeNode *node = st.top();
+                if (node->right != NULL && lastVisited != node->right) {
+                    curr = node->right;
+                } else {
+                    res.push_back(node->val);
+                    lastVisited = node;
+                    st.pop();
+                }
+            }
+        }
+        return res;
     }
 };
