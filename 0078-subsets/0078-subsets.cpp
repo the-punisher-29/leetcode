3357baaf2da9133c4e48f215dee980a874692149
Soumen Kumar <soumenkumar9503@gmail.com>
2024-09-12 01:33:31 +0530

    Time: 0 ms (100%), Space: 8.5 MB (77.23%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,17 +3,16 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
         vector<int> currentSubset;
-        backtrack(nums, 0, currentSubset, result);
+        bt(nums, 0, currentSubset, result);
         return result;
     }
 private:
-    void backtrack(vector<int>& nums, int start, vector<int>& currentSubset, vector<vector<int>>& result) {
-        result.push_back(currentSubset); // Add the current subset to the result
-
+    void bt(vector<int>& nums, int start, vector<int>& currentSubset, vector<vector<int>>& result) {
+        result.push_back(currentSubset);
         for (int i = start; i < nums.size(); i++) {
-            currentSubset.push_back(nums[i]); // Include the current element
-            backtrack(nums, i + 1, currentSubset, result); // Explore the next element
-            currentSubset.pop_back(); // Backtrack: remove the current element to try others
+            currentSubset.push_back(nums[i]);
+            bt(nums, i + 1, currentSubset, result);//going to next ele
+            currentSubset.pop_back();//removing curr and exp aaspas
         }
     }
 };
