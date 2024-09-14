f03440186739bfa4c38775e2648402f25f86482c
Soumen Kumar <soumenkumar9503@gmail.com>
2024-09-14 17:47:07 +0530

    Time: 444 ms (33.8%), Space: 69.9 MB (87.62%) - LeetHub

---
+++ 3072-reshape-data-pivot/3072-reshape-data-pivot.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    result = weather.pivot(index='month', columns='city', values='temperature')
+    return result
+    
