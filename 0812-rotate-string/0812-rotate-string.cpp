37f774b52e50cd7519992c09d033a81dd35faaab
Soumen Kumar <soumenkumar9503@gmail.com>
2024-11-04 04:17:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (36.98%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,7 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) return false;
         if (s.length() == 0) return true;
-        
+        //simple--we can concatenate the given s and if the goal is in there then true else false
         string doubled = s + s;
         return doubled.find(goal) != string::npos;
     }
