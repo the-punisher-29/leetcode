4514924fc6c373449f3ec37e67093eba804153b0
Soumen Kumar <soumenkumar9503@gmail.com>
2025-02-03 00:35:26 +0530

    Time: 11 ms (48.29%), Space: 16 MB (47.62%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // Convert wordDict to unordered_set for O(1) lookup
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        
+        // dp[i] means if s[0:i] can be segmented into words from dictionary
+        vector<bool> dp(s.length() + 1, false);
+        dp[0] = true;  // Empty string is always valid
+        
+        // For each position in string
+        for (int i = 1; i <= s.length(); i++) {
+            // Try all possible words ending at position i
+            for (int j = 0; j < i; j++) {
+                // If s[0:j] is valid and s[j:i] is in dictionary
+                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[s.length()];
+    }
+};
