e97eb5328562f911766d33f1ee7772f05268b5e1
Soumen Kumar <soumenkumar9503@gmail.com>
2025-02-12 00:07:48 +0530

    Time: 3 ms (32.43%), Space: 9.6 MB (13.98%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        vector<char> stack;
-        int partLen = part.length();
+        vector<char>st;
+        int pl= part.length();
         for(char c : s) {
-            stack.push_back(c);
-            if(stack.size() >= partLen) {
-                bool isMatch = true;
-                for(int i = 0; i < partLen; i++) {
-                    if(stack[stack.size() - partLen + i] != part[i]) {
+            st.push_back(c);
+            if(st.size()>= pl) {
+                bool isMatch=true;
+                for(int i = 0; i < pl; i++) {
+                    if(st[st.size()-pl+i]!=part[i]) {
                         isMatch = false;
                         break;
                     }
                 }
-                
-                // If match found, remove part
                 if(isMatch) {
-                    for(int i = 0; i < partLen; i++) {
-                        stack.pop_back();
+                    for(int i = 0; i <pl; i++) {
+                        st.pop_back();
                     }
                 }
             }
         }
-        
-        // Convert stack back to string
-        return string(stack.begin(), stack.end());
+        return string(st.begin(), st.end());
     }
 };
