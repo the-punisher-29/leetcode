d2c6524e147319fe7018d42b741934b09065bec5
Soumen Kumar <soumenkumar9503@gmail.com>
2025-05-19 23:52:36 +0530

    Time: 0 ms (100%), Space: 22.9 MB (65.15%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        //bc obv triangle nhi hoga
+        if (nums[0] + nums[1] <= nums[2] || 
+            nums[0] + nums[2] <= nums[1] || 
+            nums[1] + nums[2] <= nums[0]) {
+            return "none";
+        }
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+};
