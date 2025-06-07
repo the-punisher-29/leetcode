bd150fca9c35c9b588cc2f0ce190d0d8b1a9606f
Soumen Kumar <soumenkumar9503@gmail.com>
2025-06-08 00:00:31 +0530

    Time: 1634 ms (5.19%), Space: 20.6 MB (100%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        vector<int> freq(26, 0); // frequency of each character
+        string stack;
+        
+        for (char c : s) {
+            if (c == '*') {
+                if (!stack.empty()) {
+                    // Find the smallest character that exists
+                    for (int i = 0; i < 26; i++) {
+                        if (freq[i] > 0) {
+                            char minChar = 'a' + i;
+                            // Remove the rightmost occurrence of this character
+                            for (int j = stack.length() - 1; j >= 0; j--) {
+                                if (stack[j] == minChar) {
+                                    stack.erase(stack.begin() + j);
+                                    freq[i]--;
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            } else {
+                stack.push_back(c);
+                freq[c - 'a']++;
+            }
+        }
+        
+        return stack;
+    }
+};
