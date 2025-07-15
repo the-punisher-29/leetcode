6f9c3cbb52eed426663bd3cab60b785af5957d71
Soumen Kumar <soumenkumar9503@gmail.com>
2025-07-16 02:40:29 +0530

    Time: 0 ms (100%), Space: 8 MB (67.67%) - LeetHub

--- 3396-valid-word/3396-valid-word.cpp
+++ 3396-valid-word/3396-valid-word.cpp
@@ -2,13 +2,18 @@ class Solution {
  public:
   bool isValid(string word) {
     //C++20-flex
+    //range
     return word.length() >= 3 &&
-           ranges::all_of(word, [](char c) { return isalnum(c); }) &&
-           ranges::any_of(word, isVowel) && ranges::any_of(word, isConsonant);
+           ranges::all_of(word, [](char c) { return isalnum(c); }) 
+           &&
+           ranges::any_of(word, isVowel) 
+           && 
+           ranges::any_of(word, isConsonant);
   }
  private:
   static bool isVowel(char c) {
     static constexpr string_view kVowels = "aeiouAEIOU";
+    //learnt this string_view(non owning useful in function parameters,copying is cheap(aka mem effi))
     return kVowels.find(c) != string_view::npos;
   }
   static bool isConsonant(char c) {
